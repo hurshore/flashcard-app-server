ae8c41b2fd62907e658c79e7fbaa1eeafe98a09b
Tofunmi Osho <oshotofunmi@gmail.com>
2021-06-21 04:31:30 +0100

    implement authentication

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const authRoutes = require('./routes/authRoutes');
+const app = express();
+
+app.use(cors());
+app.use(express.static('.'));
+app.use(express.json());
+dotenv.config();
+
+mongoose.connect(
+  process.env.DB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to db');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server up and running');
+    });
+  }
+);
+
+// Routes
+app.use('/api/user', authRoutes);
